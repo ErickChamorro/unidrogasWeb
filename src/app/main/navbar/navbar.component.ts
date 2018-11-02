d8afddec283eb7cad5957617494eeeefd85e21eb
ErickChamorro <eschamorro97@misena.edu.co>
2018-11-02 11:39:29 -0500

    integracion de navbar y sidemenu listo falta definir lista en el menu

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -11,5 +11,23 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
   }
+  /* Set the width of the side navigation to 250px */
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('la-navbar').style.marginLeft = '250px';
+    document.getElementById('main').style.transition = 'all 0.3s ease-in-out';
+    document.getElementById('la-navbar').style.transition = 'all 0.3s ease-in-out';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+    document.body.style.transition = 'all 0.3s ease-in-out';
+  }
+
+  /* Set the width of the side navigation to 0 */
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+    document.getElementById('la-navbar').style.marginLeft = '0';
+    document.body.style.backgroundColor = 'white';
+  }
 
 }
